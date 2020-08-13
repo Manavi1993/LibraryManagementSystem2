b1aa5bc4ffa2d0eb26fe5eb6a802b063b556e803
Thakur, Manavi <manavithakur99@rediffmail.com>
2020-08-13 07:20:37 +0530

    new changes

---
+++ LibraryManagementSystem/database/librarymanagementdatabasescripts.sql
@@ -0,0 +1,127 @@
+USE [master]
+GO
+
+/****** Object:  Database [Library_Management_System]    Script Date: 8/12/2020 11:23:13 AM ******/
+CREATE DATABASE [Library_Management_System]
+ CONTAINMENT = NONE
+ ON  PRIMARY 
+( NAME = N'Library_Management_System', FILENAME = N'C:\Users\612999\Library_Management_System.mdf' , SIZE = 8192KB , MAXSIZE = UNLIMITED, FILEGROWTH = 65536KB )
+ LOG ON 
+( NAME = N'Library_Management_System_log', FILENAME = N'C:\Users\612999\Library_Management_System_log.ldf' , SIZE = 8192KB , MAXSIZE = 2048GB , FILEGROWTH = 65536KB )
+GO
+
+IF (1 = FULLTEXTSERVICEPROPERTY('IsFullTextInstalled'))
+begin
+EXEC [Library_Management_System].[dbo].[sp_fulltext_database] @action = 'enable'
+end
+GO
+
+ALTER DATABASE [Library_Management_System] SET ANSI_NULL_DEFAULT OFF 
+GO
+
+ALTER DATABASE [Library_Management_System] SET ANSI_NULLS OFF 
+GO
+
+ALTER DATABASE [Library_Management_System] SET ANSI_PADDING OFF 
+GO
+
+ALTER DATABASE [Library_Management_System] SET ANSI_WARNINGS OFF 
+GO
+
+ALTER DATABASE [Library_Management_System] SET ARITHABORT OFF 
+GO
+
+ALTER DATABASE [Library_Management_System] SET AUTO_CLOSE ON 
+GO
+
+ALTER DATABASE [Library_Management_System] SET AUTO_SHRINK OFF 
+GO
+
+ALTER DATABASE [Library_Management_System] SET AUTO_UPDATE_STATISTICS ON 
+GO
+
+ALTER DATABASE [Library_Management_System] SET CURSOR_CLOSE_ON_COMMIT OFF 
+GO
+
+ALTER DATABASE [Library_Management_System] SET CURSOR_DEFAULT  GLOBAL 
+GO
+
+ALTER DATABASE [Library_Management_System] SET CONCAT_NULL_YIELDS_NULL OFF 
+GO
+
+ALTER DATABASE [Library_Management_System] SET NUMERIC_ROUNDABORT OFF 
+GO
+
+ALTER DATABASE [Library_Management_System] SET QUOTED_IDENTIFIER OFF 
+GO
+
+ALTER DATABASE [Library_Management_System] SET RECURSIVE_TRIGGERS OFF 
+GO
+
+ALTER DATABASE [Library_Management_System] SET  ENABLE_BROKER 
+GO
+
+ALTER DATABASE [Library_Management_System] SET AUTO_UPDATE_STATISTICS_ASYNC OFF 
+GO
+
+ALTER DATABASE [Library_Management_System] SET DATE_CORRELATION_OPTIMIZATION OFF 
+GO
+
+ALTER DATABASE [Library_Management_System] SET TRUSTWORTHY OFF 
+GO
+
+ALTER DATABASE [Library_Management_System] SET ALLOW_SNAPSHOT_ISOLATION OFF 
+GO
+
+ALTER DATABASE [Library_Management_System] SET PARAMETERIZATION SIMPLE 
+GO
+
+ALTER DATABASE [Library_Management_System] SET READ_COMMITTED_SNAPSHOT OFF 
+GO
+
+ALTER DATABASE [Library_Management_System] SET HONOR_BROKER_PRIORITY OFF 
+GO
+
+ALTER DATABASE [Library_Management_System] SET RECOVERY SIMPLE 
+GO
+
+ALTER DATABASE [Library_Management_System] SET  MULTI_USER 
+GO
+
+ALTER DATABASE [Library_Management_System] SET PAGE_VERIFY CHECKSUM  
+GO
+
+ALTER DATABASE [Library_Management_System] SET DB_CHAINING OFF 
+GO
+
+ALTER DATABASE [Library_Management_System] SET FILESTREAM( NON_TRANSACTED_ACCESS = OFF ) 
+GO
+
+ALTER DATABASE [Library_Management_System] SET TARGET_RECOVERY_TIME = 60 SECONDS 
+GO
+
+ALTER DATABASE [Library_Management_System] SET DELAYED_DURABILITY = DISABLED 
+GO
+
+ALTER DATABASE [Library_Management_System] SET QUERY_STORE = OFF
+GO
+
+USE [Library_Management_System]
+GO
+
+ALTER DATABASE SCOPED CONFIGURATION SET LEGACY_CARDINALITY_ESTIMATION = OFF;
+GO
+
+ALTER DATABASE SCOPED CONFIGURATION SET MAXDOP = 0;
+GO
+
+ALTER DATABASE SCOPED CONFIGURATION SET PARAMETER_SNIFFING = ON;
+GO
+
+ALTER DATABASE SCOPED CONFIGURATION SET QUERY_OPTIMIZER_HOTFIXES = OFF;
+GO
+
+ALTER DATABASE [Library_Management_System] SET  READ_WRITE 
+GO
+
+
